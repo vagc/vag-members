6a223f9ddf8a296d12c1262cdd5950fc81dfc080
Martin Ndeto <martindeto@gmail.com>
2023-03-31 07:08:11 +0300

    Update data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
-
 Sticker.create({"member_id"=>16, "sticker_number"=>950, "sticker_variation"=>"white"})
 Sticker.create({"member_id"=>48, "sticker_number"=>117, "sticker_variation"=>"black"})
 Sticker.create({"member_id"=>22, "sticker_number"=>919, "sticker_variation"=>"black"})
 Sticker.create({"member_id"=>28, "sticker_number"=>590, "sticker_variation"=>"black"})
 Sticker.create({"member_id"=>37, "sticker_number"=>896, "sticker_variation"=>"white"})
+Sticker.create({"member_id"=>13, "sticker_number"=>624, "sticker_variation"=>"black"})
